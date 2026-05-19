64f84adfc2df574a60848ce7d44696111d2e06b6
Corey Tarpley <developertarpley@gmail.com>
2026-05-18 20:41:42 -0700

    updated content area: display list and card

--- task-manager/src/pages/display/Display_Tasks.jsx
+++ task-manager/src/pages/display/Display_Tasks.jsx
@@ -2,19 +2,16 @@ import "./display_tasks.css";
 import Use_Tasks from "../../state/Use_Tasks";
 import { useEffect } from "react";
 
+import Task_Card from "../../components/task-card/Task_Card";
+
 export default function ({ tasks }) {
     useEffect(() => {
     }, [tasks]);
 
     return <>
-        <h2>Current Plan's</h2>
-        <ul className="task-list">
-            {
-                tasks.map(task =>
-                    <li key={task.id} className="list-item">
-                        {task.text}
-                    </li>)
-            }
-        </ul>
+        <h2 style={{
+            margin:"10px"
+        }}>Current Plan's</h2>
+        <Task_Card tasks={tasks} />
     </>
 }
