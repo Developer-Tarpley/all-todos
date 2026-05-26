d2da462d3bd38eadb89ce883274a42b8085cb029
Corey Tarpley <developertarpley@gmail.com>
2026-05-26 15:29:20 -0700

    fixed edit logic and added a task delagation and completed toggle logic

--- task-manager/src/components/buttons/Delete_Button.jsx
+++ task-manager/src/components/buttons/Delete_Button.jsx
@@ -2,16 +2,12 @@ import "./delete_button.css";
 import { FaTrashCan } from "react-icons/fa6";
 
 export default function Delete_Button({ deleteTask }) {
-    const handleDelete = (event) => {
-        let li = event.target.closest("li");
-        console.log("LI: ", li)
-        deleteTask(li.dataset.id)
-    }
+
     return <>
         <FaTrashCan
-            onClick={(event) => handleDelete(event)}
             className="remove-task-button"
             role="button"
+            data-action= "remove"
             tabIndex={1}
         />
     </>
--- task-manager/src/components/inputs/Custom_Checkbox.jsx
+++ task-manager/src/components/inputs/Custom_Checkbox.jsx
@@ -1,46 +1,25 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./custom_checkbox.css";
 
-export default function Custom_Checkbox(props) {
-  const [check, setCheck] = useState(null)
-
-  const handleChange = (event) => {
-    console.log("is checked", event.target.checked)
-    console.log("is checked", event.target.closest("li"))
-    const li = event.target.closest("li")
-
-    if (!check) {
-      setCheck(event.target.checked)
-      li.style.textDecoration = "line-through";
-      li.style.color = "green";
-      li.textContent += " Completed!"
-      
-    } else {
-      setCheck(false)
-      li.style.textDecoration = "none";
-      li.style.color = "black";
-    }
-  }
+export default function Custom_Checkbox({ task }) {
+  // console.log(task)
 
   return (
     <>
       {
-        check ?
+        task.completed ?
           <label className="container">
-            <input onChange={(event) => handleChange(event)} type="checkbox" />
+            <input type="checkbox" defaultChecked />
             <span className="checkmark"></span>
             <span className="label-text"></span>
           </label>
           :
-
           <label className="container">
-            <input onChange={(event) => handleChange(event)} type="checkbox" />
+            <input type="checkbox" />
             <span className="checkmark"></span>
-            <span className="label-text">{props.text}</span>
+            <span className="label-text"></span>
           </label>
       }
     </>
   )
-
-
 };
