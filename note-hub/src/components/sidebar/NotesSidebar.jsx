ebc12f2c31667f22dac2830772368ee1c9c6588b
Corey Tarpley <developertarpley@gmail.com>
2026-05-27 09:05:59 -0700

    created-notes-sidebar-layout

--- note-hub/src/components/sidebar/NotesSidebar.jsx
+++ note-hub/src/components/sidebar/NotesSidebar.jsx
@@ -1,5 +1,18 @@
 
+import useNotes from "../../hooks/useNotes";
+import SidebarHeader from "./SidebarHeader";
+import NewNoteButton from "./NewNoteButton";
+import NotesList from "./NotesList";
 
-export default function NotesSidebar(){
-    return <></>
+export default function NotesSidebar() {
+    const { notes, activeNoteId } = useNotes();
+
+    return <aside className="notes-sidebar-container">
+        <SidebarHeader />
+        <NewNoteButton />
+        <NotesList
+            notes={notes}
+            activeNoteId={activeNoteId}
+        />
+    </aside>
 }
