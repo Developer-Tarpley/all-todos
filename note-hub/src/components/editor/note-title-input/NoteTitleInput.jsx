3fad64d5b1dba06f928fbe31c06393965cf9d858
Corey Tarpley <developertarpley@gmail.com>
2026-05-30 15:18:35 -0700

    implemented a controlled input for note title

--- note-hub/src/components/editor/note-title-input/NoteTitleInput.jsx
+++ note-hub/src/components/editor/note-title-input/NoteTitleInput.jsx
@@ -11,12 +11,22 @@
 
 // ---
 
+
+
+import { useState } from "react";
 import { useNotes } from "../../../hooks/useNotes"
-export default function NoteTitleInput({note}) {
-    let {updateNote} = useNotes();
+
+export default function NoteTitleInput({ note }) {
+    let { updateNote } = useNotes();
+
+
+    const handleUpdate = (value) => {
+        updateNote(note.id, { title: value })
+    }
+
     return <input
         type="text"
         value={note.title}
-        onChange={()=>updateNote(note.id, {title: note.title})}
+        onChange={(event) => handleUpdate(event.target.value)}
     />
 }
