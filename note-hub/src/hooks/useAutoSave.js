2091753858c8a10de6d88bc249e53d2a0793b1ac
Corey Tarpley <developertarpley@gmail.com>
2026-05-30 20:17:08 -0700

    created autosave functionality

--- note-hub/src/hooks/useAutoSave.js
+++ note-hub/src/hooks/useAutoSave.js
@@ -1,5 +1,6 @@
 // ---
 
+
 // ### **FEAT‑122 — Implement `useAutoSave()`**
 // **Description:**  
 // Hook that watches title/body and triggers updateNote.
@@ -11,3 +12,44 @@
 // - Update AutoSaveIndicator state  
 
 // ---
+import { useEffect } from "react";
+
+import { useNotes } from "./useNotes";
+import { useDebounce } from "./useDebounce";
+import AutoSaveIndicator from "../components/editor/auto-save-indicator/AutoSaveIndicator"
+import { useState } from "react";
+
+export function useAutoSave() {
+    let { notes, activeNoteId, updateNote } = useNotes();
+    let [status, setStatus] = useState("idle");
+
+    const activeNote = notes.find(note=>note.id === activeNoteId);
+    if(!activeNote) return "idle";
+
+    const debouncedTitle = useDebounce(activeNote.title, 500);
+    const debouncedBody = useDebounce(activeNote.body, 500);
+
+    useEffect(()=>{
+        setStatus("saving");
+    },[activeNote.title, activeNote.body])
+
+    useEffect(() => {
+
+        try{
+
+            updateNote(activeNote.id,{
+                title: debouncedTitle,
+                body: debouncedBody
+            });
+
+            setStatus("saved");
+        }
+        catch(error){
+            setStatus("Oops! could't save");
+            console.log("auto save error: ", error);
+        }
+
+    },[debouncedTitle, debouncedBody]);
+
+    return status;
+}
